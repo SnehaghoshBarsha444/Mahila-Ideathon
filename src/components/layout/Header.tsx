8354402dd9f2e17aaa03846392a475022c7794e2
Sneha Ghosh (Technical_Isopod_075) <153223798+SnehaghoshBarsha444@users.noreply.github.com>
2025-04-01 22:16:39 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -35,7 +35,6 @@ export function Header() {
             <img 
               src="public/favicon.ico" 
               alt="FemFin Connect Logo"
-              className="w-10 h-10 rounded-full"
             />
             <span className="font-display font-semibold text-xl">FemFin Connect</span>
           </Link>
